7e325af27ab5458d5754e3d8b641922ef39ab157
fineiskid <ffineis@uw.edu>
2018-02-19 23:47:01 -0600

    fix tree sender cpp, fix up embedding code

--- fbboost/src/send_through_tree.cpp
+++ fbboost/src/send_through_tree.cpp
@@ -2,6 +2,18 @@
 using namespace Rcpp;
 
 // [[Rcpp::export]]
+int c_intvec_which_equals(IntegerVector x, int val){
+  int idx=-1;
+  int n = x.size();
+  for(int j = 0; j < n; ++j) {
+    if(x[j] == val){
+      idx = j;
+    }
+  }
+  return idx;
+}
+
+// [[Rcpp::export]]
 int ThroughTree(List x,
                     Rcpp::IntegerVector Node,
                     Rcpp::CharacterVector Feature,
@@ -9,37 +21,45 @@ int ThroughTree(List x,
                     Rcpp::IntegerVector Yes,
                     Rcpp::IntegerVector No,
                     Rcpp::IntegerVector Missing) {
+  int rowIdx = 0;
   int node = 0;
   bool notLeaf = TRUE;
   double split = NA_REAL;
   double xVal = NA_REAL;
   std::string feat;
 
-  feat = Rcpp::as<std::string>(Feature[node]);
-  notLeaf = TRUE;
-
   while (notLeaf) {
-    feat = Feature[node];
-    
+    // Rcout << "preceding rowIdx is " << rowIdx << std::endl;
+    feat = Feature[rowIdx];
+    // Rcout << "feat is " << feat << std::endl;
+
     if (feat == "Leaf"){
       notLeaf = FALSE;
     } else{
-      split = Split[node];
+      split = Split[rowIdx];
       xVal = x[feat];
+      // Rcout << "split is " << split << std::endl;
+      // Rcout << "xVal is " << xVal << std::endl;
       
-      if (ISNA(xVal)){
-        node = Missing[node];
+      if (NumericVector::is_na(xVal)){
+        // Rcout << "xVal is missing " << std::endl;
+        node = Missing[rowIdx];
       } else{
+        // Go to row where Node ID is Yes[node]
         if (xVal < split){
-          node = Yes[node] - 1;
+          // Rcout << "Go Left" << std::endl;
+          node = Yes[rowIdx];
         } else {
-          node = No[node] - 1;
+          // Rcout << "Go Right" << std::endl;
+          node = No[rowIdx];
         }
       }
     }
+    // Rcout << "node is " << node << std::endl;
+    rowIdx = c_intvec_which_equals(Node, node);
   }
     
-  return node + 1;
+  return node;
 }
 
 
@@ -49,10 +69,10 @@ int ThroughTree(List x,
 # x <- irisDT[1]
 # x[1, 'Petal.Width'] <- NA_real_
 # ThroughTree(x
-#             , Node = c(1, 2, 3)
+#             , Node = c(0, 1, 2)
 #             , Feature = c('Petal.Width', 'Leaf', 'Leaf')
 #             , Split = c(2.45, NA_real_, NA_real_)
-#             , Yes = c(2, NA_integer_, NA_integer_)
-#             , No = c(3, NA_integer_, NA_integer_)
-#             , Missing = c(2, NA_integer_, NA_integer_))
+#             , Yes = c(1, NA_integer_, NA_integer_)
+#             , No = c(2, NA_integer_, NA_integer_)
+#             , Missing = c(1, NA_integer_, NA_integer_))
 */
